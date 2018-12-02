240b35792149612da48258056a9639173eae8c0c
Anthony Onwuasoanya <onwuaa@lime-h.cs.rpi.edu>
2018-12-02 15:20:08 -0500

    Accepts list of links from Link Analysis instead of single link

--- Sender.py
+++ Sender.py
@@ -1,9 +1,12 @@
 import requests
+import re
 
 TEXT = 'text_transformation'
 LINK = 'link_analysis'
 INDEX = 'indexing'
 DEFAULT_HEADERS = { 'user-agent': 'LimeH-Crawler' }
+ACCESS_TIME = r'[Dd]ate:\s?'
+FORWARD_ADDRESS = ''
 
 # Send message to text transformation, link analysis or indexing depending on response crawler received
 class Sender:
@@ -24,19 +27,33 @@ class Sender:
     def send_request(target, headers, content):
         payload = {}
         url = ''
+        access_time = ''
         # 2XX success, send to text transformation
         if target == TEXT:
-            payload = {}
-            url = ''
+            url = 'url'
+            for line in str(headers).split('\n'):
+                if re.match(ACCESS_TIME, line):
+                    access_time = re.sub(ACCESS_TIME, "", line)
+                    print(line)
+            forward_address = 'forward_address'
+            payload = {
+                    'metadata': {
+                        'url': url,
+                        'access_time': access_time,
+                        'forward address': forward_address
+                        }
+                    }
+           # print(payload)
+           # print(headers)
         # 4XX error, notify link analysis
         elif target == LINK:
             payload = {}
-            url = ''
+            url = 'http://green-eth.cs.rpi.edu/crawling'
         # 4XX error, notify indexing
         elif target == INDEX:
             payload = {}
             url = ''
         else:
             return None
-       return  requests.post(url, payload)
+        #return requests.post(url, payload)
 
